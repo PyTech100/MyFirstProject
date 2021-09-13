4a5701f88ea258fa8cd1aad0320a1912a5ee2fe7
PyTech100 <90625629+PyTech100@users.noreply.github.com>
2021-09-13 16:45:07 +0300

    Upload MongoDB example file

---
+++ mongo.py
@@ -0,0 +1,16 @@
+
+from pymongo import MongoClient
+import pprint as pretty
+import certifi
+
+ca = certifi.where()
+
+client = MongoClient("mongodb+srv://Ayelet:Ayelet25@pythonatdell."
+                     "go6jh.mongodb.net/PythonAtDell?retryWrites=true&w=majority", tlsCAFile=ca)
+db = client.test
+server_status = db.command("serverStatus")
+pretty.pprint(server_status)
+
+
+
+
